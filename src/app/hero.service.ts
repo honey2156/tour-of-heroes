c8e081f5954f8dc6ce26dff95c753edbccefebbf
Mandeep Singh <mandeep.singh@nagarro.com>
2018-05-09 12:16:48 +0530

    services completed

---
+++ src/app/hero.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './heroes';
+import { HEROES } from './mock-heroes';
+
+@Injectable()
+export class HeroService {
+
+  constructor() { }
+
+  getHeroes(): Promise<Hero[]> {
+    return Promise.resolve(HEROES)
+  }
+
+}
